898e1209ad749ff21f31222defc83b2d924bc916
Martin Olsson <martinolsson89@gmail.com>
2024-03-21 21:58:42 +0100

    Add project files.

---
+++ UserRegistration.Tests/UserRegistrationService.Tests.cs
@@ -0,0 +1,11 @@
+namespace UserRegistration.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UserRegistration/UserRegistrationService.cs
@@ -0,0 +1,7 @@
+﻿namespace UserRegistration
+{
+    public class UserRegistrationService
+    {
+
+    }
+}
