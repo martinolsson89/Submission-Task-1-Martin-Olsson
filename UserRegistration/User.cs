008ab2e57763ef70ca49f8118b6f268644df7903
Martin Olsson <martinolsson89@gmail.com>
2024-03-21 22:06:06 +0100

    added User class

---
+++ UserRegistration/User.cs
@@ -0,0 +1,16 @@
+﻿namespace UserRegistration;
+
+public class User
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public User(string userName, string password, string email)
+    {
+        UserName = userName;
+        Password = password;
+        Email = email;
+    }
+
+}
