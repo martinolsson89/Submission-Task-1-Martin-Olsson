53cd6ad645abacc06252c666ec6ff59c7f3c647a
Martin Olsson <martinolsson89@gmail.com>
2024-03-22 16:17:37 +0100

    validating username, password and email with tests

--- UserRegistration.Tests/UserRegistrationService.Tests.cs
+++ UserRegistration.Tests/UserRegistrationService.Tests.cs
@@ -5,7 +5,7 @@ namespace UserRegistration.Tests
     {
         [TestMethod]
 
-        public void RegisterUser_AddUserInformation_ShouldReturnTrueAndCountOfOne()
+        public void RegisterUser_AddUserInformation_ShouldReturnTrueAndValidationOfDataAdded()
         {
             // Arrange
             var service = new UserRegistrationService();
@@ -18,7 +18,10 @@ namespace UserRegistration.Tests
 
             // Assert
             Assert.IsTrue(result);
-            Assert.AreEqual(1, service._users.Count);
+            Assert.AreEqual(userName, service.GetUser(userName));
+            Assert.AreEqual(password, service.GetUserByPassword(password));
+            Assert.AreEqual(email, service.GetUserByEmail(email));
+
         }
 
         // Username must be between 5 and 20 characters long, only alphanumeric characters are allowed.
@@ -56,6 +59,75 @@ namespace UserRegistration.Tests
             // Assert
             Assert.IsFalse(result);
         }
+
+        [DataRow("pass")]
+        [DataRow("password123")]
+        [DataRow("")]
+        [DataTestMethod]
+        public void ValidatePassword_WithInvalidPassword_ShouldReturnFalse(string password)
+        {
+            // Arrange
+            var service = new UserRegistrationService();
+
+            // Act
+            var result = service.ValidatePassword(password);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        // Password must be at least 8 characters long and contain at least one special character.
+        [DataRow("Password!")]
+        [DataRow("!#¤%&/())=?")]
+        [DataRow("1234567#")]
+        [DataTestMethod]
+        public void ValidatePassword_WithValidPassword_ShouldReturnTrue(string password)
+        {
+            // Arrange
+            var service = new UserRegistrationService();
+
+            // Act
+            var result = service.ValidatePassword(password);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        // Email must contain an @ symbol and .com OR .se OR .net OR .org
+        [DataRow("")]
+        [DataRow("john.doe")]
+        [DataRow("john.doe@")]
+        [DataRow("john.doe@gmail")]
+        [DataTestMethod]
+        public void ValidateEmail_WithInvalidEmail_ShouldReturnFalse(string email)
+        {
+            // Arrange
+            var service = new UserRegistrationService();
+
+            // Act
+            var result = service.ValidateEmail(email);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [DataRow("john.doe@gmail.com")]
+        [DataRow("mary.jane@yahoo.net")]
+        [DataRow("kim@hotmail.se")]
+        [DataRow("jim@email.org")]
+        [DataTestMethod]
+        public void ValidateEmail_WithValidEmail_ShouldReturnTrue(string email)
+        {
+            // Arrange
+            var service = new UserRegistrationService();
+
+            // Act
+            var result = service.ValidateEmail(email);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
     }
 
 }
--- UserRegistration/UserRegistrationService.cs
+++ UserRegistration/UserRegistrationService.cs
@@ -4,7 +4,7 @@ namespace UserRegistration
 {
     public class UserRegistrationService
     {
-        public List<User> _users = new List<User>();
+        private readonly List<User> _users = [];
 
         public bool RegisterUser(string userName, string password, string email)
         {
@@ -12,6 +12,27 @@ namespace UserRegistration
             _users.Add(user);
 
             return true;
+        }
+
+        // Returns the user with the given username
+        public string GetUser(string userName)
+        {
+            var user = _users.FirstOrDefault(u => u.UserName == userName)!;
+            return user.UserName;
+        }
+
+        // Returns the user with the given email
+        public string GetUserByEmail(string email)
+        {
+            var user = _users.FirstOrDefault(u => u.Email == email)!;
+            return user.Email;
+        }
+
+        // Returns the user with the given password
+        public string GetUserByPassword(string password)
+        {
+            var user = _users.FirstOrDefault(u => u.Password == password)!;
+            return user.Password;
         }
 
         //Username must be between 5 and 20 characters long, only alphanumeric characters are allowed
@@ -28,6 +49,41 @@ namespace UserRegistration
             }
             
             if (!Regex.IsMatch(userName, @"^[a-zA-Z0-9]+$"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+
+            //Email must contain an @ symbol and .com OR .se OR .net OR .org
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
             {
                 return false;
             }
